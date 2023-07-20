537043f3ce8ad7f4cf7e604e00ff062a8ba40146
ViperZer0 <viperzer0thehackingreptile@gmail.com>
2023-07-20 15:56:59 -0700

    Basic board movement now works
    Pieces snap to the center of the square they're moved to.
    Pieces jump back to starting position if dropped off board.
    Pieces can be dragged and dropped in general.

---
+++ board/Board.cs
@@ -0,0 +1,176 @@
+using Godot;
+using System;
+
+public partial class Board : Node2D
+{
+	private uint numRanks = 8;
+	private uint numFiles = 8;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		FitBoardToScreen();
+		// Change the board size whenever the screen size changes.
+		GetViewport().SizeChanged += FitBoardToScreen;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	/// <summary>
+	/// "Snaps" the x, y coordinates given to the nearest board space.
+	/// </summary>
+	/// <param name="coords">The coordinates</param>
+	/// <returns>A tuple representing a rank (up and down) and file (left to
+	/// right) starting at 0 representing A8 (black is at 0), which is weird.</returns>
+	public (uint rank, uint file) GetNearestBoardSpace(Vector2 coords)
+	{
+		// We expect this to be checked beforehand.
+		if (!CoordsOnBoard(coords))
+		{
+			throw new ArgumentOutOfRangeException("Coordinates were outside of board space!");
+		}
+
+		// Get coordinates given the top left board corner is (0,0).
+		Vector2 relativeCoords = coords - GetTopLeftBoardCorner();
+		relativeCoords /= GetSpaceSize();
+		Vector2 closestSpace = relativeCoords.Floor();
+		// X and Y should both be integers due to floor, and shouldn't be
+		// negative due to previous check.
+		return ((uint)closestSpace.Y, (uint)closestSpace.X);
+	}
+
+	/// <summary>
+	/// Given coordinates, determines whether the mouse is on board or not.
+	/// If not we want to "invalidate" the move.
+	/// </summary>
+	/// <param name="coords">Vector2 coords to check against.</param>
+	/// <returns>True if the coordinates are on the board, false otherwise.</returns>
+	public bool CoordsOnBoard(Vector2 coords)
+	{
+		if(coords < GetTopLeftBoardCorner())
+		{
+			return false;
+		}
+		if(coords > (GetTopLeftBoardCorner() + GetBoardSize()))
+		{
+			return false;
+		}
+		return true;
+	}
+
+	/// <summary>
+	/// Returns the coordinates of the center of a square.
+	/// </summary>
+	/// <param name="rank">The rank of the square (up and down the board)</param>
+	/// <param name="file">The file of the square (forward and back)</param>
+	/// <returns>The x,y coordinates of the center of the given square.</returns>
+	public Vector2 GetSpaceCoords(uint rank, uint file)
+	{
+		if (rank >= numRanks)
+		{
+			throw new ArgumentOutOfRangeException($"Rank greater than {numRanks}");
+		}
+		if (file >= numFiles)
+		{
+			throw new ArgumentOutOfRangeException($"File greater than ${numFiles}");
+		}
+		return (new Vector2(file, rank) * GetSpaceSize() + GetCenterOfSpaceOffset());
+	}
+
+	/// <summary>
+	/// Returns the coordinates of the center of a square given its space name
+	/// (i.e "C3", "A2")
+	/// </summary>
+	/// <param name="spaceName">The name of the space, i.e "A5"</param>
+	/// <returns>The coordinates of the center of the square.</returns>
+	public Vector2 GetSpaceCoords(string spaceName)
+	{
+		return new Vector2(0, 0);
+	}
+
+	/// <summary>
+	/// Converts a rank and file index to a space name.
+	/// </summary>
+	/// <param name="rank">The index of the rank.</param>
+	/// <param name="file">The index of the file.</param>
+	/// <returns>The string coordinates (i.e "A3")</returns>
+	public string RankAndFileIndexToSpaceName(uint rank, uint file)
+	{
+		return "A8";
+	}
+
+	/// <summary>
+	/// Converts a space name to a rank and file index tuple.
+	/// </summary>
+	/// <param name="spaceName">The string representation of the space name (i.e
+	/// "A5")</param>
+	/// <returns>A tuple containing the rank and file index.</returns>
+	public (uint rank, uint file) SpaceNameToRankAndFile(string spaceName)
+	{
+		return (0, 0);
+	}
+
+	// Call this when the board resizes (to move the pieces)
+	[Signal]
+	public delegate void BoardResizedEventHandler();
+
+
+	// Get the size of each space in pixels (global?)
+	public Vector2 GetSpaceSize()
+	{
+		return GetBoardSize() / new Vector2(numFiles, numRanks);
+	}
+
+	// Offset to top left space center using global coords?
+	private Vector2 GetCenterOfSpaceOffset()
+	{
+		Vector2 relativeSizeOfSpace = GetSpaceSize();
+		Vector2 leftTopCornerOfBoard = GetTopLeftBoardCorner();
+		return leftTopCornerOfBoard + relativeSizeOfSpace/2;
+	}
+
+	// In number of pixels, but scaled according to the size of the sprite.
+	private Vector2 GetBoardSize()
+	{
+		Sprite2D sprite = GetNode<Sprite2D>("BoardSprite");
+		Texture2D texture = sprite.Texture;
+		return texture.GetSize() * sprite.Scale;
+	}
+
+	private Vector2 GetTextureSize()
+	{
+		Texture2D texture = GetNode<Sprite2D>("BoardSprite").Texture;
+		return texture.GetSize();
+	}
+
+	// Global position (in pixels) of TL corner of board.
+	private Vector2 GetTopLeftBoardCorner()
+	{
+		return this.GlobalPosition - GetBoardSize() / 2;
+	}
+
+	// Resizes the board to fit in the screen centered.
+	private void FitBoardToScreen()
+	{		
+		//Fit the board to the screen.
+		Vector2 size = GetViewport().GetVisibleRect().Size;
+
+		// Get the smallest of the sizes of the viewport.
+		float minimumSize = size[(int)size.MinAxisIndex()];
+
+		Sprite2D sprite = GetNode<Sprite2D>("BoardSprite");
+
+		// We're assuming the board is square so x and y are the same.
+		float scaleFactor = minimumSize / GetTextureSize().X;
+
+		// Scale the board.
+		sprite.Scale = new Vector2(scaleFactor, scaleFactor);
+		
+		// Then center it?
+		this.Position = GetViewport().GetVisibleRect().Position + GetViewport().GetVisibleRect().Size / 2;
+		EmitSignal(SignalName.BoardResized);
+	}
+}
--- piece/Piece.cs
+++ piece/Piece.cs
@@ -3,13 +3,113 @@ using System;
 
 public partial class Piece : Node2D
 {
+	// Piece is grabbed only when these both are true.
+	private bool mouseOver = false;
+	private bool pickedUp = false;
+	
+	[Export]
+	private uint rank = 0;
+	private uint file = 0;
+
+	[Export]
+	private Board board;
+
+	// How much padding the edge of the piece should have to the edge of the
+	// square.
+	[Export]
+	private float marginScale = 0.9f;
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Area2D area = GetNode("PieceSprite").GetNode<Area2D>("GrabArea");
+		area.MouseEntered += OnMouseEnter;
+		area.MouseExited += OnMouseExit;
+		// Not sure if this check is necessary?
+		if(board == null)
+		{
+			board = GetParent().GetNode<Board>("Board");
+		}
+
+		board.BoardResized += OnBoardResized;
+
+		ResizePieceToBoard();
+		MoveToSquare(this.rank, this.file);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (this.pickedUp)
+		{
+			this.Position = GetViewport().GetMousePosition();
+		}
+	}
+
+	// Called when an input event happens.
+	public override void _Input(InputEvent @event)
+	{
+		if(@event.IsActionPressed("click") && this.mouseOver)
+		{
+			this.pickedUp = true;
+		}
+		if(@event.IsActionReleased("click") && this.pickedUp)
+		{
+			this.pickedUp = false;
+			if(!this.board.CoordsOnBoard(this.Position))
+			{
+				// Move the piece back to where it was.
+				MoveToSquare(this.rank, this.file);
+			}
+			else
+			{
+				// Move to the new space
+				(uint rank, uint file) = this.board.GetNearestBoardSpace(this.Position);
+				MoveToSquare(rank, file);
+			}
+		}
+	}
+
+	private void MoveToSquare(uint rank, uint file)
+	{
+		this.Position = this.board.GetSpaceCoords(rank, file);
+		this.rank = rank;
+		this.file = file;
+	}
+
+	private void ResizePieceToBoard()
+	{
+		Vector2 spaceSize = this.board.GetSpaceSize();
+
+		float minimumSize = spaceSize[(int)spaceSize.MinAxisIndex()];
+
+		Sprite2D sprite = GetNode<Sprite2D>("PieceSprite");
+
+		// We will assume that the board is perfectly square.
+		float scaleFactor = minimumSize / sprite.Texture.GetSize().X * this.marginScale;
+
+		sprite.Scale = new Vector2(scaleFactor, scaleFactor);
+
+		// Reposition it.
+		MoveToSquare(this.rank, this.file);
+	}
+
+	// Event handler calld when a mouse enters the piece area.
+	private void OnMouseEnter()
+	{
+		this.mouseOver = true;
+	}
+
+	// Event handler called when a mouse exits the piece area.
+	private void OnMouseExit()
+	{
+		this.mouseOver = false;
+	}
+
+	private void OnBoardResized()
+	{
+		ResizePieceToBoard();
 	}
 }
+
